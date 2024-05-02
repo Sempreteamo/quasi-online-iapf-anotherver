55824615dd7b585ed43e2c45cba430d85376d0e3
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-02 15:19:44 +0100

    Add files via upload

---
+++ evaluate_psi.R
@@ -0,0 +1,10 @@
+evaluate_psi <- function(x, psi_pa, t, n){ 
+  if(t == (n + 1)){
+    psi_t <- 0
+  }else{
+    dif <- x - psi_pa[t, 1:d]
+    psi_t <- -(d/2)*log(2*pi) - (1/2)*log(det(diag(psi_pa[t, (d+1):(d+d)], nrow=d,ncol=d))) +						
+      (-1/2)*t(dif)%*%diag((psi_pa[t, (d+1):(d+d)])^(-1), nrow=d,ncol=d)%*%dif
+  }
+  return(psi_t)
+}
