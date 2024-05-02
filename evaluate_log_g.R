01c680375ba0fe4c0ecc968402b5570c86cf6cc8
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-02 15:54:13 +0100

    Update evaluate_log_g.R

--- evaluate_log_g.R
+++ evaluate_log_g.R
@@ -1,4 +1,5 @@
 evaluate_log_g <- function(y, x){  
-  log_density <- (-d/2)*log(2*pi) - (1/2)*log(det(D)) + (-1/2)*t(y-C%*%x)%*%D%*%(y-C%*%x)
+  dif <- y-C%*%x
+  log_density <- (-d/2)*log(2*pi) - (1/2)*log(det(D)) + (-1/2)*t(dif)%*%D%*%dif
   return (log_density) #obs prob  C%*%x = x
 }
