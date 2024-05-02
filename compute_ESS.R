beb8e124d9fcedf009713b055d366d92dc3d7a0e
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-02 16:09:32 +0100

    Update compute_ESS.R

--- compute_ESS.R
+++ compute_ESS.R
@@ -2,7 +2,7 @@
 
 #logW_un = w[t-1,]
 
-ESS_log <- function(logW_un){
+compute_ESS_log <- function(logW_un){
     logW <- normalise_weights_in_log_space(logW_un)
     ess <- 1/sum(logW^2)
   return(ess)  
