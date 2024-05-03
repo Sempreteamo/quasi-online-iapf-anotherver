c9eaec43d1d45dda237a6616c7cb23e0bd655743
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-03 12:43:02 +0100

    Update parameters.R

--- parameters.R
+++ parameters.R
@@ -10,3 +10,10 @@ for (i in 1:d){
 ini_mu <- rep(0, d)
 B = C = D = ini_cov = diag(1, nrow = d, ncol = d)
 model <- list(ini_mu, ini_cov, A, B, C, D)
+
+
+lag = 10
+len = 200
+blocks <- generate_blocks(lag, len)[[1]]
+obs <- generate_obs()
+data <- list(blocks, obs)
