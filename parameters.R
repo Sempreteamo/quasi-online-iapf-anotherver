ce0811393e41fca635ea78a51ceb6624a65ba7a5
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-03 02:52:24 +0100

    Add files via upload

---
+++ parameters.R
@@ -0,0 +1,12 @@
+
+d = 3
+alpha = 0.42
+A <- matrix(nrow = d, ncol = d)
+for (i in 1:d){
+  for (j in 1:d){
+    A[i,j] = alpha^(abs(i-j) + 1)
+  }
+}
+ini_mu <- rep(0, d)
+B = C = D = ini_cov = diag(1, nrow = d, ncol = d)
+model <- list(ini_mu, ini_cov, A, B, C, D)
