71e83d8231b8b53c4bcd19c23ac2734560625f5a
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-07 00:22:24 +0100

    Update and rename compute_ESS.R to compute_ESS_log.R

--- compute_ESS_log.R
+++ compute_ESS_log.R
@@ -3,7 +3,7 @@
 #logW_un = w[t-1,]
 
 compute_ESS_log <- function(logW_un){
-    logW <- normalise_weights_in_log_space(logW_un)
+    logW <- normalise_weights_in_log_space(logW_un)[[1]]
     ess <- 1/sum(logW^2)
   return(ess)  
 }
