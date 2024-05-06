3798f190e7a07dc23434aeb37efe22291634feb9
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-06 15:21:51 +0100

    Update and rename generate_obs.R to sample_obs.R

---
+++ sample_obs.R
@@ -0,0 +1,18 @@
+#param <- list(A, B, C, D)
+#N = Time
+sample_obs <- function(param, N){
+  A <- param[[1]]
+  B <- param[[2]]
+  C <- param[[3]]
+  D <- param[[4]]
+  
+  X <-  matrix(0, nrow = N, ncol = d)
+  
+  X[1,] <- rnorm(d)     
+  
+  for(t in 2:Time){ 
+    X[t,] <- chol(B)%*%rnorm(d) + A%*%X[t-1,]  #t(rmvn(d) + A%*%x)
+  }
+  data <- sample_g(list(C, D), X, N)
+  return(data)
+}
