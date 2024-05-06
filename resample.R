bb4228ae4fef121f266fe00d33f16d311f1533eb
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-07 00:30:59 +0100

    Update resample.R

--- resample.R
+++ resample.R
@@ -2,23 +2,23 @@
 #Z_apf = Z_apf[l]
 #N = N[l]
 resample <- function(logW, mode = 'res'){
-  mx <- max(logW)
-  w_ <- exp(logW - mx)/sum(exp(logW - mx))
+  
+  w_ <- normalise_weights_in_log_space(logW)[[1]]
   
   if(mode == 'multi'){
     indices <- sample(1:N, N, replace = TRUE, prob = w_)
     return(indices)
     
   }else{
-    Ntm <- as.integer(Num*w_)
+    Ntm <- as.integer(N*w_)
     
-    indices <- unlist(lapply(1:Num, function(i) {rep(i, Ntm[i])}))
-    mr <- Num - sum(Ntm)
+    indices <- unlist(lapply(1:N, function(i) {rep(i, Ntm[i])}))
+    mr <- N - sum(Ntm)
     
-    w_hat <- w_ - Ntm/Num
-    w_hat <- w_hat*Num/mr
+    w_hat <- w_ - Ntm/N
+    w_hat <- w_hat*N/mr
     
-    indices <- c(sample(1:Num, mr, replace = TRUE, prob = w_hat), indices)
+    indices <- c(sample(1:N, mr, replace = TRUE, prob = w_hat), indices)
     return(indices)
   }
 }
