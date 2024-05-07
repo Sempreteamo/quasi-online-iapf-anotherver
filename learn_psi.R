f66d82dacaa78607f94b95fbfb83970d131eb521
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-07 03:51:38 +0100

    Update learn_psi.R

--- learn_psi.R
+++ learn_psi.R
@@ -1,14 +1,15 @@
 #N=N[l], x = X_apf[t,,]
 
-learn_psi <- function(n, x, N, L){
+learn_psi <- function(x, N, Time){
   psi <- matrix(NA, nrow = Time, ncol = N)
-  params <- matrix(NA, nrow = Time, ncol = 2*d)
+  psi_pa <- matrix(NA, nrow = Time, ncol = 2*d)
   
   #calculate psi
-  for(t in n:(n-L+1)){
-    if(t == n){
+  for(t in Time:1){
+    print(t)
+    if(t == Time){
       for(i in 1:N){
-        dif <- x[t,i,] - obs[t,]
+        dif <- x[t, i,] - obs[t,]
         
         psi[t,i] <- (1 / ((2 * pi)^(d / 2))) * 
           exp(-0.5 * t(dif) %*% dif)
@@ -18,22 +19,24 @@ learn_psi <- function(n, x, N, L){
     }else{
       for(i in 1:N){
         
-        psi[t,i] <- exp(g(obs[t,],x[t,i,]))*evaluate_psi_tilda(x[t,i,], params, t, n)
+        psi[t,i] <- exp(evaluate_log_g(list(C, D), x[t,i,], obs[t,]))*
+          exp(evaluate_psi_tilda(x[t,i,], psi_pa, list(A, B), Time))
           
-          #(2*pi)^(-d/2)*prod(params[t+1, (d+1):(d+d)]+1)^(-1/2)*
-          #exp(-(1/2)*t(A%*%x[t,i,] - params[t+1, 1:d])%*%diag((params[t+1, (d+1):(d+d)]+diag(B))^(-1), nrow=d,ncol=d)%*%
-           #     (A%*%x[t,i,] - params[t+1, 1:d]) )
+          #(2*pi)^(-d/2)*prod(psi_pa[t+1, (d+1):(d+d)]+1)^(-1/2)*
+          #exp(-(1/2)*t(A%*%x[t,i,] - psi_pa[t+1, 1:d])%*%diag((psi_pa[t+1, (d+1):(d+d)]+diag(B))^(-1), nrow=d,ncol=d)%*%
+           #     (A%*%x[t,i,] - psi_pa[t+1, 1:d]) )
         
       }
     }
     
-    params[t,] <- optimization(x[t,,], log(psi[t,]))
+    print(1)
+    psi_pa[t,] <- optimization(X_apf[t,,], log(psi[t,]))
     
     
-    #print(params[t, 1:d])
+    #print(psi_pa[t, 1:d])
     #print(obs[t,])
     
   }
-  return(params)
+  return(params = psi_pa)
   
 }
