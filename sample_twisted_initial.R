ead4d2157e90dc81446c94d3629d0c84ef546ecd
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-11 06:35:22 +0100

    Update sample_twisted_initial.R

--- sample_twisted_initial.R
+++ sample_twisted_initial.R
@@ -2,15 +2,13 @@
 #psi <- psi_pa[t,]
 sample_twisted_initial <- function(params, psi, N){
   
-  output <- compute_twisted_initial_params(params, psi)
+  output <- compute_twisted_params(params, psi)
   
   mu <- output[[1]]
   
   cov <- output[[2]]
   
-  param <- list(mu, cov)
-  
-  samples <- sample_normal_distribution(param, N)
+  samples <- rmvn(N, mu, cov)
   
   return(samples)
 }
