4f9d9fedf2069086e719eb00d259809948e0fcad
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-11 06:16:10 +0100

    Update sample_obs.R

--- sample_obs.R
+++ sample_obs.R
@@ -1,18 +1,21 @@
 #param <- list(A, B, C, D)
 #N = Time
-sample_obs <- function(param, N){
-  A <- param[[1]]
-  B <- param[[2]]
-  C <- param[[3]]
-  D <- param[[4]]
+sample_obs <- function(model, N){
+  A <- model$A
+  B <- model$B
+  C <- model$C
+  D <- model$D
   
-  X <-  matrix(0, nrow = N, ncol = d)
+  X <- matrix(0, nrow = N, ncol = d)
+  data <- matrix(0, nrow = N, ncol = d)
   
   X[1,] <- rnorm(d)     
   
   for(t in 2:Time){ 
     X[t,] <- chol(B)%*%rnorm(d) + A%*%X[t-1,]  #t(rmvn(d) + A%*%x)
   }
-  data <- sample_normal_distribution(list(t(C%*%t(X)), D), N)
+  for(t in 1:N){
+    data[t,] <- rmvn(1, C%*%X[t,], D)
+  }
   return(data)
 }
