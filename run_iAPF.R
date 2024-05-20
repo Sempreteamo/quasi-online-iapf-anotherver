a8ace1bb9fa30d86bea90dc6466a887db6faede4
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-20 03:24:53 +0100

    Update run_iAPF.R

--- run_iAPF.R
+++ run_iAPF.R
@@ -11,7 +11,69 @@ run_iAPF <- function(model, data, Napf){
       l = 1
       Z_apf <- vector()
       N[l] = Napf    
-      output <- run_psi_APF(model, list(obs[breaks[[index]][(b-1)]:(breaks[[index]][b]-1),], breaks[[index]][(b-1):b], 0, 0), N[l], psi_pa = 0, init = TRUE)
+      output <- run_psi_APF(model, list(obs[breaks[[index]][(b-1)]:(breaks[[index]][b]-1),], breaks[[index]][(b-1):b], run_iAPF <- function(model, data, Napf){
+        breaks <- data$breaks
+        psi_index <- data$psi_index
+        obs <- data$obs
+        Time <- nrow(obs)
+        psi_pa1 = psi_pa2 = NULL
+        
+        for(index in 1:2){
+          
+          for(b in 2:length(breaks[[index]])){
+            l = 1
+            Z_apf <- vector()
+            N[l] = Napf    
+            output <- run_psi_APF(model, list(obs[breaks[[index]][(b-1)]:(breaks[[index]][b]-1),], breaks[[index]][(b-1):b], w_apf[nrow(w_apf),], X_apf[nrow(X_apf),,]), N[l], psi_pa = 0, init = TRUE)
+            X_apf <- output[[1]]
+            w_apf <- output[[2]]
+            Z_apf[l] <- output[[3]]
+            
+            while(TRUE){
+              
+              output <- list()
+              
+              if(l != 1){
+                #print(l)
+                #generate filtering particles X_apf for psi the next iteration
+                #APF outputs filtering X_apf for the next psi, and smoothing X_apf_s
+                #for the final calculation
+                
+                output <- run_psi_APF(model, list(obs[breaks[[index]][(b-1)]:(breaks[[index]][b]-1),], breaks[[index]][(b-1):b], w_apf[nrow(w_apf),], X_apf[nrow(X_apf),,]), 
+                                      N[l], psi_pa, init = FALSE)
+                X_apf <- output[[1]]
+                w_apf <- output[[2]]
+                Z_apf[l] <- output[[3]]
+                ancestors <- output[[4]]
+              }
+              
+              #to speed up the algorithm, I just fix the number of iterations to be k.
+              #Here k = 5
+              
+              if(l <= k ){
+                print(l)
+                #receive filtering particles X_apf for psi
+                psi_pa <- learn_psi(X_apf, N[l], obs[breaks[[index]][(b-1)]:(breaks[[index]][b]-1),], model) 
+                
+                if(l > k & N[max(l-k,1)] == N[l] & is.unsorted(Z_apf[max(l-k,1):l])){  
+                  N[l+1] <- 2*N[l]
+                  
+                }else{
+                  N[l+1] <- N[l]
+                }
+                
+                l <- l+1
+              }else break
+            }
+            
+            psi_pa1 <- cbind(psi_pa1, psi_pa)
+          }
+          
+        }
+        #output psi
+        return(list(X_apf, w_apf, psi_pa, Z_apf[l], ancestors))
+      }
+      , 0), N[l], psi_pa = 0, init = TRUE)
       X_apf <- output[[1]]
       w_apf <- output[[2]]
       Z_apf[l] <- output[[3]]
@@ -60,3 +122,4 @@ run_iAPF <- function(model, data, Napf){
   #output psi
   return(list(X_apf, w_apf, psi_pa, Z_apf[l], ancestors))
 }
+
