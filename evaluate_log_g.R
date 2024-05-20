31b9c8841cfc79a15e2726a053d9dd8567e5ec40
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-21 00:10:56 +0100

    Update evaluate_log_g.R

--- evaluate_log_g.R
+++ evaluate_log_g.R
@@ -1,12 +1,19 @@
 #params <- list(C, D)
-evaluate_log_g <- function(params, x, datum){  
-  C <- params[[1]]
-  D <- params[[2]]
+evaluate_log_g <- function(model, x, datum){  
+  C <- model$C
+  D <- model$D
   dif <- datum - C%*%x
-  log_density <- (-d/2)*log(2*pi) - (1/2)*log(det(D)) + (-1/2)*t(dif)%*%D%*%dif
-  return (log_density)
+  log_density <- (-d/2)*log(2*pi) - (1/2)*log(prod(diag(D))) + (-1/2)*t(dif)%*%D%*%dif
+  return (log_density) 
 }
 
 #test
-#x <- rnorm(d)
-#datum <- rnorm(d)
+#x <- c(0.7418182, 0.1584118)
+#datum <- c(0.67968786, -0.01167335)
+#expected_output <- dmvn(datum, C%*%x, D, log = TRUE)
+#result <- evaluate_log_g(model, x, datum)
+#tolerance <- 1e-6
+#is_close_enough <- abs(result - expected_output) < tolerance
+#if (!is_close_enough) {
+# stop("Test failed: Incorrect output from optimization function.")
+#}
