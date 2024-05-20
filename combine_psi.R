d4317d3b53b0f84eee79e1041fda73db7409a12e
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-05-20 22:19:07 +0100

    Update combine_psi.R

--- combine_psi.R
+++ combine_psi.R
@@ -1,6 +1,6 @@
 combine_psi <- function(psi_pa, data){
   index <- data$psi_index
-  obx <- data$obs
+  obs <- data$obs
   Time <- nrow(obs)
   
   combined_psi <- matrix(NA, Time, 2*d)
@@ -12,6 +12,10 @@ combine_psi <- function(psi_pa, data){
     return(psi = combined_psi)
 }
 
-#test: psi_pa1 <- matrix(rnorm(Time*2*d), Time, 2*d)
-#psi_pa2 <- matrix(rnorm(Time*2*d), Time, 2*d)
+#test: 
+#psi_pa1 <- matrix(1:10, 10, 1)
+#psi_pa2 <- matrix(11:20, 10, 1)
 #psi_pa <- list(psi_pa1, psi_pa2)
+#obs_ <- matrix(rnorm(10), 10, 1)
+#index <- sample(1:2, 10, replace = TRUE)
+#combine_psi(psi_pa, list(obs = obs_, psi_index = index))
