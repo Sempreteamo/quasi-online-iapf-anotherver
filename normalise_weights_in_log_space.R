e599fe539a834468111fe6be4559e77715078511
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-06-03 14:08:41 +0100

    Update normalise_weights_in_log_space.R

--- normalise_weights_in_log_space.R
+++ normalise_weights_in_log_space.R
@@ -3,6 +3,6 @@ normalise_weights_in_log_space <- function(logW_un){
   w_centered <- exp(logW_un - mx)
   sum_wc <- sum(w_centered)
   logW <- w_centered/sum_wc
-  logZ <- log(sum_wc / length(logW_un)) + mx
+  logZ <- log(sum_wc) - log(length(logW_un)) + mx
   return(list(logW, logZ))
 }
