92be9c3ecac8a572c46289cab41ef1645ca4c761
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-06-03 12:51:21 +0100

    Update combine_psi.R

--- combine_psi.R
+++ combine_psi.R
@@ -11,10 +11,3 @@ combine_psi <- function(psi, index){
     
     return(psi = combined_psi)
 }
-
-#test: 
-#psi1 <- matrix(1:10, 10, 1)
-#psi2 <- matrix(11:20, 10, 1)
-#psi <- list(psi1, psi2)
-#index <- sample(1:2, 10, replace = TRUE)
-#combine_psi(psi, index)
