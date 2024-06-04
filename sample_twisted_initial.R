93562c5c7cef0b36957e143d1588d6b6f0f18e74
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-06-04 04:32:21 +0100

    Update sample_twisted_initial.R

--- sample_twisted_initial.R
+++ sample_twisted_initial.R
@@ -2,15 +2,12 @@
 #psi <- psi_pa[t,]
 sample_twisted_initial <- function(model, psi, N){
   
-  ini_mu <- model$ini_mu
-  ini_cov <- model$ini_cov
-  
-  output <- compute_twisted_params(list(ini_mu, ini_cov), psi)
+  output <- compute_twisted_params(model, psi)
   
   mu <- output[[1]]
   
   cov <- output[[2]]
-  
+  #set.seed(1234)
   samples <- rmvn(N, mu, cov)
   
   return(samples)
