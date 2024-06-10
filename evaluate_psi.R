e7355b16a72bd4f9a189cb35aa62ceab29894675
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-06-10 01:21:03 +0100

    Update evaluate_psi.R

--- evaluate_psi.R
+++ evaluate_psi.R
@@ -2,7 +2,7 @@
 #psi_x <- 0
 #psi_pa <- psi_pa[t,]
 evaluate_psi <- function(x, psi_pa){ 
-  
+    d <- length(x)
     dif <- x - psi_pa[1:d]
     psi_x <- -(d/2)*log(2*pi) - (1/2)*log(det(diag(psi_pa[(d+1):(d+d)], nrow=d,ncol=d))) +						
       (-1/2)*t(dif)%*%diag((psi_pa[(d+1):(d+d)])^(-1), nrow=d,ncol=d)%*%dif
