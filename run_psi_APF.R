624ca824a7dd1762b44b4d5c579bd2c0174c33cd
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-06-11 10:54:33 +0100

    Update run_psi_APF.R

--- run_psi_APF.R
+++ run_psi_APF.R
@@ -1,25 +1,34 @@
 run_psi_APF <- function(model, data, N, psi_pa, init){ #purely filtering particles
+  ini_mu <- model$ini_mu
+  ini_cov <- model$ini_cov
   A <- model$A
   B <- model$B
   C <- model$C
   D <- model$D
+  d <- model$d
   obs <- data[[1]]
   breaks <- data[[2]]
   w_previous <- data[[3]]
   X_previous <- data[[4]]
   Time <- nrow(obs)
   kappa <- model$kappa
+  ancestors <- matrix(NA, Time, N)
     
     X <- array(NA, dim = c(Time, N, d))
     w <- matrix(NA, Time, N)
     logZ <- 0
+    #print('start')
     
     if(init){
       if(breaks[1] == 1){
+        #set.seed(1234)
         X[1,,] <- rnorm(N*d)  
         for(i in 1:N){
           w[1,i] <- evaluate_log_g(model, X[1,i,], obs[1,])  
         }
+        
+        #print('finish 1')
+        
       }else{
         
         s <- resample(w_previous, mode = 'multi')
@@ -29,21 +38,24 @@ run_psi_APF <- function(model, data, N, psi_pa, init){ #purely filtering particl
         }
       }
       
+      ancestors[1,] <- seq(1:N) 
+      
       for(t in 2:Time){
-        
         if(compute_ESS_log(w[t-1,]) <= kappa*N){
           
-          ancestors <- resample(w[t-1,])
+          ancestors[t,] <- resample(w[t-1,], mode = 'multi')
           logZ = logZ + normalise_weights_in_log_space(w[t-1,])[[2]]
           # at the initialization stage, we want filtering particles for psi
-          
+          #set.seed(1234)
           for(i in 1:N){
-            X[t,i,] <- rmvn(1, A%*%X[t-1, ancestors[i],], B)
+            
+            X[t,i,] <- rmvn(1, A%*%X[t-1, ancestors[t,i],], B)
             w[t,i] <- evaluate_log_g(model, X[t,i,], obs[t,])  
           }
           
         }else{
-          
+          #set.seed(1234)
+          ancestors[t,] <- ancestors[t-1,] 
           for(i in 1:N){
             X[t,i,] <- rmvn(1, A%*%X[t-1, i,], B)
             w[t,i] <- w[t-1,i] + evaluate_log_g(model, X[t,i,], obs[t,])  
@@ -51,14 +63,20 @@ run_psi_APF <- function(model, data, N, psi_pa, init){ #purely filtering particl
         }
       }
       
+      #print('finish 2')
+      
       logZ <- logZ + normalise_weights_in_log_space(w[Time,])[[2]]
       
     }else{
       if(breaks[1] == 1){
-        X[1,,] <- sample_twisted_initial(model, psi_pa[1,], N)
+        
+        X[1,,] <- sample_twisted_initial(list(mean = ini_mu, cov = ini_cov), psi_pa[1,], N)
+        
         for(i in 1:N){
           w[1,i] <- eval_twisted_potential(model, list(psi_pa[1,], psi_pa[2,], psi_pa[1,]), X[1,i,], obs[1,])
         }
+        
+        #print('finish  1 plus')
       }else{
         #adjust the weights -- might need a single function
         w_adj <- vector()
@@ -79,21 +97,21 @@ run_psi_APF <- function(model, data, N, psi_pa, init){ #purely filtering particl
         
       }
       
+      ancestors[1,] <- seq(1:N) 
       for(t in 2:(Time-1)){
-        #print(t)
         if(compute_ESS_log(w[t-1,]) <= kappa*N){
           
-          ancestors <- resample(w[t-1,])
+          ancestors[t,] <- resample(w[t-1,], mode = 'multi')
           logZ = logZ + normalise_weights_in_log_space(w[t-1,])[[2]]
           
           for(i in 1:N){
             #filtering particles
-            X[t,i,] <- sample_twisted_transition(X[t-1, ancestors[i],], model, psi_pa[t,], 1)
+            X[t,i,] <- sample_twisted_transition(X[t-1, ancestors[t,i],], model, psi_pa[t,], 1)
             w[t,i] <- eval_twisted_potential(model, list(NA, psi_pa[t+1,], psi_pa[t,]), X[t,i,], obs[t,])
             
           }
         }else{
-          
+          ancestors[t,] <- ancestors[t-1,]
           for(i in 1:N){
             #filtering particles
             X[t,i,] <- sample_twisted_transition(X[t-1, i,], model, psi_pa[t,], 1)
@@ -102,22 +120,23 @@ run_psi_APF <- function(model, data, N, psi_pa, init){ #purely filtering particl
         }
         
       }
+      #print('finish 2 plus')
       
       t = Time
       
       if(compute_ESS_log(w[t-1,]) <= kappa*N){
         
-        ancestors <- resample(w[t-1,])
+        ancestors[t,] <- resample(w[t-1,])
         logZ = logZ + normalise_weights_in_log_space(w[t-1,])[[2]]
         
         for(i in 1:N){
           #filtering particles
-          X[t,i,] <- sample_twisted_transition(X[t-1, ancestors[i],], model, psi_pa[t,], 1)
+          X[t,i,] <- sample_twisted_transition(X[t-1, ancestors[t,i],], model, psi_pa[t,], 1)
           w[t,i] <- eval_twisted_potential(model, list(NA, NA, psi_pa[t,]), X[t,i,], obs[t,])
           
         }
       }else{
-        
+        ancestors[t,] <- ancestors[t-1,]
         for(i in 1:N){
           #filtering particles
           X[t,i,] <- sample_twisted_transition(X[t-1, i,], model, psi_pa[t,], 1)
@@ -125,11 +144,12 @@ run_psi_APF <- function(model, data, N, psi_pa, init){ #purely filtering particl
         }
       }
       
+      #print('finish Time plus')
       logZ <- logZ + normalise_weights_in_log_space(w[Time,])[[2]] 
     }
     
     
-  
+  ##print(ancestors)
   
   return(list(X, w, logZ, ancestors))
 }
