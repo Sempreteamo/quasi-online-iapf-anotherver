0c3f2e012f7f0042f7baf99c603238c71895992b
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-06-17 15:20:10 +0100

    Update evaluate_psi_tilde.R

--- evaluate_psi_tilde.R
+++ evaluate_psi_tilde.R
@@ -1,6 +1,17 @@
-#if the time t of psi_pa[t+1,] == Time, we evaluate psi_pa to be NA to make psi_tilde = 0
-#psi_pa <- psi_pa[t+1,]
-evaluate_psi_tilde <- function(x, psi_pa, model){  #from 0 to T. 0,T = 1 
+#' Function to evaluate the density of a twisting function
+#'
+#'This function evaluates the one-step-ahead expectation of a twisting function with 
+#'the specified parameters at one or more points.
+#'
+#' @param x Points to evalute the function at
+#' @param psi_pa Parameters of twisting function
+#' @param model List containing model parameters
+#' 
+#' @return Twisting function psi-tilde evaluated at specified points
+#' @export
+#'
+evaluate_psi_tilde <- function(x, psi_pa, model){  
+  d <- length(x)
   A <- model$A
   B <- model$B
 
@@ -9,6 +20,7 @@ evaluate_psi_tilde <- function(x, psi_pa, model){  #from 0 to T. 0,T = 1
   psi_tilde <- (-d/2)*log(2*pi) - (1/2)*log(det(diag(psi_pa[(d+1):(d+d)] + diag(B), nrow=d, ncol=d))) +
     (-1/2)*t(dif)%*%diag((psi_pa[(d+1):(d+d)] + diag(B))^(-1), nrow=d,ncol=d)%*%dif
   
+  #if t+1 = Time, we set psi_pa to be NA to make psi_tilde = 0
   if(is.na(psi_tilde)){
     psi_tilde <- 0
   }
@@ -16,13 +28,3 @@ evaluate_psi_tilde <- function(x, psi_pa, model){  #from 0 to T. 0,T = 1
   return(psi_tilde)
 }
 
-#test:
-#x <- c(0.7418182, 0.1584118)
-#psi_pa <- c(-0.8921799, -0.4000272, 1.000000, 1.0000000)
-#expected_output <- dmvn(as.vector(A%*%x), psi_pa[1:d], diag(psi_pa[(d+1):(d+d)] + diag(B), nrow=d, ncol=d), log = TRUE)
-#result <- evaluate_psi_tilde(x, psi_pa, model)
-#tolerance <- 1e-6
-#is_close_enough <- abs(result - expected_output) < tolerance
-#if (!is_close_enough) {
-# stop("Test failed: Incorrect output from optimization function.")
-#}
