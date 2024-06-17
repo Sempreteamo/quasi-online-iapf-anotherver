0b454393168de980e78de3a85ec59735a962f11f
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-06-17 11:50:03 +0100

    Update sample_obs.R

--- sample_obs.R
+++ sample_obs.R
@@ -1,10 +1,19 @@
-#param <- list(A, B, C, D)
-#N = Time
+#' Function to sample observations
+#'
+#'This function samples synthetic observations for each time step given the state space model. 
+#'
+#' @param model List containing model parameters
+#' @param N Number of observations
+#'
+#' @return A list containing the observation sequence
+#' @export
+#'
 sample_obs <- function(model, N){
   A <- model$A
   B <- model$B
   C <- model$C
   D <- model$D
+  d <- model$d
   
   X <- matrix(0, nrow = N, ncol = d)
   data <- matrix(0, nrow = N, ncol = d)
@@ -12,7 +21,7 @@ sample_obs <- function(model, N){
   X[1,] <- rnorm(d)     
   
   for(t in 2:Time){ 
-    X[t,] <- chol(B)%*%rnorm(d) + A%*%X[t-1,]  #t(rmvn(d) + A%*%x)
+    X[t,] <- chol(B)%*%rnorm(d) + A%*%X[t-1,]  
   }
   for(t in 1:N){
     data[t,] <- rmvn(1, C%*%X[t,], D)
