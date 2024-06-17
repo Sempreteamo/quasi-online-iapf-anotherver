a80fb13550af8bc46071ff9c11bf80c0bed3c479
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-06-17 14:22:00 +0100

    Update normalise_weights_in_log_space.R

--- normalise_weights_in_log_space.R
+++ normalise_weights_in_log_space.R
@@ -1,3 +1,17 @@
+#' Function to calculate the normalised weights and the normalising constant
+#'
+#'This function takes a vector of unnormalised weights and normalises them in the usual numerically 
+#'stable way. It returns the normalised weights as well as the normalising constant. 
+#'The inputs and outputs are all in log-space.
+#'
+#' @param logW_un Log of the vector of unnormalised weights
+#'
+#' @return A list with two elements:
+#' logW is log of the vector of self-normalised weights
+#' logZ is log of the normalising constant, i.e. of the sum of the unnormalised weights
+#' 
+#' @export
+#'
 normalise_weights_in_log_space <- function(logW_un){
   mx <- max(logW_un)
   w_centered <- exp(logW_un - mx)
