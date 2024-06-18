fa3b19780113c8074eb3b3d95bdbc0114710f4e1
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-06-18 02:02:54 +0100

    Update sample_twisted_initial.R

--- sample_twisted_initial.R
+++ sample_twisted_initial.R
@@ -1,17 +1,25 @@
-#params <- list(ini_mu, ini_cov)
-#psi <- psi_pa[t,]
-sample_twisted_initial <- function(model, psi, N){
+#' Function to provide samples from the twisted initial distribution
+#'
+#'This function samples from a twisted initial distribution specified as a Gaussian mixture.
+#'
+#' @param params List of Gaussian mixture initial parameters
+#' @param psi Parameters of the twisting function
+#' @param N Number of samples to provide
+#'
+#' @return Samples from the twisted initial distribution
+#' @export
+#'
+sample_twisted_initial <- function(params, psi, N){
   
-  output <- compute_twisted_params(model, psi)
+  output <- compute_twisted_params(params, psi)
   
   mu <- output[[1]]
   
   cov <- output[[2]]
+  
   #set.seed(1234)
   samples <- rmvn(N, mu, cov)
   
   return(samples)
 }
 
-#test
-#psi <- rnorm(2*d)
