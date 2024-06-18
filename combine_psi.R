8ec6cc45f952055d30ee9e6624b2d93fa959ed30
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-06-18 09:13:27 +0100

    Update combine_psi.R

--- combine_psi.R
+++ combine_psi.R
@@ -1,3 +1,16 @@
+#' Function generating preferred psi function arguments
+#' 
+#' This function takes the parameters of the psi functions produced by each 
+#' of the two sets of iAPF and combines them to give a single preferred value at each
+#' time. Easily extensible to more than 2 iAPFs if needed.
+#'
+#' @param psi Parameters of the psi functions
+#' @param index Indicator of which psi is preferred at each time 
+#'
+#' @return List of preferred psi function arguments
+#' 
+#' @export
+#'
 combine_psi <- function(psi, index){
   dims <- dim(psi[[1]])
   Time <- dims[1]
