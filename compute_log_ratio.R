ab70d4b8512d3ec2f09d995b78d9cf9b53a9eee0
Sempreteamo <84420813+Sempreteamo@users.noreply.github.com>
2024-07-04 07:56:41 +0100

    Update compute_log_ratio.R

--- compute_log_ratio.R
+++ compute_log_ratio.R
@@ -1,15 +1,6 @@
-compute_log_ratio <- function(Z, model, data){
-  a0 <- model$a0
-  P0 <- model$P0
-  dt <- model$dt
-  ct <- model$ct
-  Tt <- model$Tt
-  Zt <- model$Zt
-  Ht <- model$Ht
-  Gt <- model$Gt
-  obs <- data$obs
-  fkf.obj <- fkf(a0, P0, dt, ct, Tt, Zt, Ht, Gt, yt = t(obs))
-  fks.obj <- fks(fkf.obj)
-  fkf.obj_Z <- fkf(a0, P0, dt, ct, Tt, Zt, Ht, Gt, yt = t(obs))$logLik
-  cat('NC = ', fkf.obj_Z, 'log_ratio = ', exp(Z-fkf.obj_Z))
+compute_log_ratio <- function(Z, fkf.obj){
+  
+  fkf.obj_Z <- fkf.obj$logLik
+  cat('NC = ', fkf.obj_Z, 'est = ', Z, 'log_ratio = ', exp(Z-fkf.obj_Z))
+  return(exp(Z-fkf.obj_Z))
 }
